7f2549be5804e08c558c7c33f4d2e50e9c86ecd1
chaos3a64 <chaos3a64@gmail.com>
2024-07-30 04:53:05 +0000

    Edited test_actions.oy

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 import tempfile
-from chaosext.actions import create_local_file
+# from chaosext.actions import create_local_file
 
 def test_create_local_file():
     # Create a temporary file
     with tempfile.NamedTemporaryFile() as tmp:
         file_path = tmp.name
         file_content = "Hello, World!" #This will be changed to pull the fake files from the folder to send out.
-        create_file_at_location(file_path, file_content)
+        create_local_file(file_path, file_content)
         assert os.path.exists(file_path)
         with open(file_path, 'r') as f:
             assert f.read() == file_content
@@ -17,10 +17,7 @@ def test_create_local_file_with_non_existent_dir():
         file_path = os.path.join(tmp_dir, 'ubdir', 'test_file.txt')
         file_content = 'This is a test file.'
 
-        config = Configuration({'dummy': 'config'})
-        secrets = Secrets({'dummy': 'secrets'})
-
-        created_file_path = create_file_at_location(file_path, file_content, config, secrets)
+        created_file_path = create_local_file(file_path, file_content)
 
         assert os.path.exists(created_file_path)
         with open(created_file_path, 'r') as f:
